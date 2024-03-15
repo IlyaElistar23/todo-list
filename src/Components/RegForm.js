29990ce26b7805258aa6d1bea83c42590bcaefe2
Ilya Zaharov <ilyaronaldo1995@gmail.com>
2024-03-15 17:18:40 +0300

    fix reg error alert

--- src/Components/RegForm.js
+++ src/Components/RegForm.js
@@ -38,7 +38,7 @@ const RegForm = ({ alertWindow, showAlert, alertProps, setAlertProps }) => {
             setAlertProps({
                 type: 'error',
                 message: 'Ошибка!',
-                description: `${error.response.data.message}.`
+                description: `${error.response.errors[0].msg}.`
             })
         }
     }
